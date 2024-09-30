a8b93ffa78e235cc1b6bedd635febe002ea4d539
Omesh Sachethana <sachethana12@gmail.com>
2024-09-30 06:36:56 +0530

    updated model

---
+++ model/item.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Item', itemSchema);
