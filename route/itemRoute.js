5dd9d3f2de98883e48f149638d5f986e0bdc67df
Omesh Sachethana <sachethana12@gmail.com>
2024-09-30 07:08:50 +0530

    added itemValidate

--- route/itemRoute.js
+++ route/itemRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const validateItem = require('../middleware/itemValidator')
 const itemController = require('../controller/itemController');
 
-router.post('/items', itemController.createItem);
+router.post('/items', validateItem, itemController.createItem);
 router.get('/items', itemController.getItems);
 router.get('/items/:id', itemController.getItem);
-router.put('/items/:id', itemController.updateItem);
+router.put('/items/:id', validateItem, itemController.updateItem);
 router.delete('/items/:id', itemController.deleteItem);
 
 module.exports = router;
